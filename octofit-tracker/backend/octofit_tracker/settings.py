f70497d203a6dd65a33dc7302f616a4354d188ab
HarukiMiura <111031779+hatomaru@users.noreply.github.com>
2025-04-08 01:09:54 +0000

    Add octofit_db to settings.py and finalize database setup

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'octofit_tracker_db',
+        'NAME': 'octofit_db',  # Name of the database
         'HOST': 'localhost',
         'PORT': 27017,
     }
